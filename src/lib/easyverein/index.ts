18f2769338cf9b035abd3012b20ad67a807c669c
Frédéric Bolvin <frederic@bol.vin>
2021-02-01 16:01:38 +0100

    upgraded packages

--- src/lib/easyverein/index.ts
+++ src/lib/easyverein/index.ts
@@ -1,22 +1,18 @@
 import { keyBy, find } from 'lodash';
 import config from './config';
-import { Client, Member } from 'easyverein';
+import { getMembers, Member, setApiToken } from 'easyverein';
 
 let membersMap: Map<string, Member> | null = null;
 let membersArr: Member[] | null = null;
 
-const client = Client(process.env.EASYVEREIN_TOKEN || '');
+setApiToken(process.env.EASYVEREIN_TOKEN || '');
 
 export const resolve = async (discordTag: string, exact: boolean = false): Promise<Member | undefined> => {
   if (!discordTag) return undefined;
   if (!membersMap) {
-    membersArr = await client.getMembers([
-      'id',
-      'contactDetails{name,companyName,dateOfBirth}',
-      'membershipNumber',
-      'memberGroups',
-      'joinDate'
-    ]);
+    membersArr = await getMembers(
+      '{id,contactDetails{name,companyName,dateOfBirth},membershipNumber,memberGroups,joinDate}'
+    );
     membersMap = new Map(Object.entries(keyBy(membersArr, (m) => m.contactDetails.companyName)));
   }
   return exact ? membersMap.get(discordTag) : find(membersArr, (m) => m.contactDetails.companyName.toLowerCase() === discordTag.toLowerCase());
